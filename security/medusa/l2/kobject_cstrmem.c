f0ed2ebc6eeccc9e71168cdbdfb7865da30718e3
PK <nestastnikos@github.com>
2020-04-18 04:09:09 -0500

    Refactoring of registry.c in L3 + small corrections in code styling

--- security/medusa/l2/kobject_cstrmem.c
+++ security/medusa/l2/kobject_cstrmem.c
@@ -4,8 +4,8 @@
  * `fetch'. Kobjects of this type are never used as a subject or object of any
  * `access' (and thus they don't need to contain object or subject vars).
  *
- * This is just slightly modified kobject_memory.c with 'update' method removed 
- * and updated returned object size 
+ * This is just slightly modified kobject_memory.c with 'update' method removed
+ * and updated returned object size
  */
 
 /* And as it isn't really necessary, it's a perfect example of loadable L2
@@ -21,8 +21,8 @@
 #include <linux/medusa/l3/registry.h>
 
 struct cstrmem_kobject {
-	pid_t  pid;		/* pid of process to read/write */
-	void * address;		/* address for read/write */
+	pid_t pid;		/* pid of process to read/write */
+	void *address;		/* address for read/write */
 	size_t size;		/* size of the data, must be <= 512 */
 	ssize_t retval;		/* either the size of data, or errno */
 	char data[512];		/* data to be written, or data read */
@@ -37,7 +37,7 @@ MED_ATTRS(cstrmem_kobject) {
 	MED_ATTR_END
 };
 
-static struct medusa_kobject_s * cstrmem_fetch(struct medusa_kobject_s *);
+static struct medusa_kobject_s *cstrmem_fetch(struct medusa_kobject_s *);
 
 MED_KCLASS(cstrmem_kobject) {
 	MEDUSA_KCLASS_HEADER(cstrmem_kobject),
@@ -48,8 +48,6 @@ MED_KCLASS(cstrmem_kobject) {
 	NULL,		/* update kobject */
 	NULL,		/* unmonitor */
 };
-
-/* module stuff */
 
 #ifdef MODULE
 static int cstrmem_kobject_unload_check(void) __exit;
@@ -67,53 +65,48 @@ int __init cstrmem_kobject_init(void)
 /* After this is called, and returns 0, cstrmem_kobject_rmmod should be. */
 static int __exit cstrmem_kobject_unload_check(void)
 {
-	if (med_unlink_kclass(&MED_KCLASSOF(cstrmem_kobject)) != MED_ALLOW)
+	if (MED_UNLINK_KCLASS(cstrmem_kobject) != 0)
 		return -EBUSY;
 	return 0;
 }
 
 void __exit cstrmem_kobject_rmmod(void)
 {
-	med_unregister_kclass(&MED_KCLASSOF(cstrmem_kobject));
+	MED_UNREGISTER_KCLASS(cstrmem_kobject);
 }
 
 module_init(cstrmem_kobject_init);
 module_exit(cstrmem_kobject_rmmod);
+
 /* quoting the comment in module.h:
  *
  * There are dual licensed components, but when running with Linux it is the
- * GPL that is relevant so this is a non issue. 
+ * GPL that is relevant so this is a non issue.
  */
 MODULE_LICENSE("GPL");
 
-/* implementation */
-
-// static struct cstrmem_kobject storage;
-
-static struct medusa_kobject_s * cstrmem_fetch(struct medusa_kobject_s * key_obj)
+static struct medusa_kobject_s *cstrmem_fetch(struct medusa_kobject_s *key_obj)
 {
 	int i, ret;
-	struct task_struct * p;
+	struct task_struct *p;
 	struct cstrmem_kobject* kobj = (struct cstrmem_kobject*) key_obj;
 
 	rcu_read_lock();
-	//p = find_task_by_pid(storage.pid);
 	p = pid_task(find_vpid(kobj->pid), PIDTYPE_PID);
 	if (p) {
 		get_task_struct(p);
 		rcu_read_unlock();
 		ret = access_process_vm(p, (unsigned long)kobj->address, kobj->data, kobj->size, 0);
-		/* TODO: here it should count characters until the first #0 found in (0,size) boundary */
+
+		/* TODO: refactor to something more .. obvious
+		 * TODO: here it should count characters until the first #0 found in (0,size) boundary */
 		for (i = 0; (i < kobj->size) && (((char*)kobj->data)[i]); i++);
-		/* end - hope it works... */
-		//free_task_struct(p);
+
 		free_task(p);
-		/* original: storage.retval = ret; */
 		kobj->retval = i;
 		return key_obj;
 	}
 	rcu_read_unlock();
-	/* subject to change */
 	kobj->retval = -ESRCH;
 	return key_obj;
 }
--- security/medusa/l3/med_l3_init.c
+++ security/medusa/l3/med_l3_init.c
@@ -6,16 +6,16 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *
  * This is the main file of L3. It calls initialization routines of
  * l3 related code, and it also exports some symbols. What a surprise,
@@ -26,15 +26,9 @@
 #include <linux/medusa/l3/arch.h>
 #include <linux/medusa/l3/registry.h>
 
-// EXPORT_SYMBOL(kclasses);
-// EXPORT_SYMBOL(evtypes);
-// EXPORT_SYMBOL(authserver);
-
 EXPORT_SYMBOL(med_register_kclass);
 EXPORT_SYMBOL(med_unregister_kclass);
 EXPORT_SYMBOL(med_get_kclass);
-EXPORT_SYMBOL(med_get_kclass_by_name);
-EXPORT_SYMBOL(med_get_kclass_by_cinfo);
 EXPORT_SYMBOL(med_get_kclass_by_pointer);
 EXPORT_SYMBOL(med_put_kclass);
 EXPORT_SYMBOL(med_unlink_kclass);
