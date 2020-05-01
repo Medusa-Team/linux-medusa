ffd9a3e956285089e267bca26df0e3e8feb17067
Roderik Ploszek <roderik.ploszek@gmail.com>
2020-05-01 18:37:14 +0200

    fix: formatting and debug output

--- security/medusa/l2/acctype_path_access.c
+++ security/medusa/l2/acctype_path_access.c
@@ -26,14 +26,14 @@ int __init path_acctype_init(void) {
 }
 
 /**
- * prepend - prepend a @str in front of current @buf pointer
+ * prepend() - prepend a @str in front of current @buf pointer
  * @buf: buffer pointer
  * @buflen: allocated length of the buffer
  * @str: string to prepend
  * @strlen: length of string to prepend
  *
- * Note 1: To prepend also terminating NULL character of @str, @strlen
- *	must be incremented by this character (i.e. by one)!
+ * Note 1: To also prepend terminating %NULL character of @str, @strlen
+ *         must be incremented by this character (i.e. by one)!
  * Note 2: Function is taken from fs/d_path.c
  */
 static int prepend(char **buf, int buflen, const char *str, int strlen)
@@ -47,15 +47,15 @@ static int prepend(char **buf, int buflen, const char *str, int strlen)
 }
 
 /**
- * medusa_get_path_from_string - prepend a @str in front of the end
- *	of newly allocated buffer
- * @str: string to place at the end of a buffer
+ * medusa_get_path_from_string() - put @str to the end of a newly allocated
+ *                                 buffer
+ * @str: string to place to the end of the buffer
  *
- * Returns newly allocated buffer with @str inserted at the end of it,
- * or NULL on allocation failure.
+ * Return: Newly allocated buffer with @str inserted to the end of it,
+ * or %NULL on allocation failure.
  *
- * Note: Buffers allocated with this function should be freed calling
- *	medusa_put_path() function.
+ * Note: Buffers allocated with this function should be freed by calling
+ *       medusa_put_path() function.
  */
 inline char *medusa_get_path_from_string(const char *str)
 {
@@ -68,13 +68,12 @@ inline char *medusa_get_path_from_string(const char *str)
 }
 
 /**
- * medusa_put_path - free a buffer used for pathname string
+ * medusa_put_path() - free buffer used for pathname string
  * @pathbuf: buffer to free
  *
- * Free a buffer allocated with __getname() and filled by
- * d_absolute_path(), which fills a buffer starting from its
- * end. So to __putname() function it should be passed the right
- * pointer obtained from __getname().
+ * Free buffer allocated with __getname() and filled by d_absolute_path(),
+ * which fills the buffer starting from its end. __putname() should receive the
+ * same pointer as obtained from __getname().
  */
 inline void medusa_put_path(char **pathbuf)
 {
@@ -85,31 +84,38 @@ inline void medusa_put_path(char **pathbuf)
 }
 
 /**
- * medusa_get_path - return a pointer to the full path
+ * medusa_get_path() - return a pointer to the full path
  * @path: path to report
- * @last: if not NULL, last element of the path
+ * @last: if not %NULL, last element of the path
  * @lasttype: type of the last element of the path
  *
- * Returns a pointer into the full pathname string or error code if an error occurs.
- * A buffer, which pointer to is returned, is allocated from the slab,
- * so callers have to free it by medusa_put_path().
+ * Return: Pointer into the full pathname string or error code if an error
+ *         occurs. A buffer to which a pointer is returned is allocated from the
+ *         slab, so callers have to free it by calling medusa_put_path().
  */
-char *medusa_get_path(const struct path *path, const struct qstr *last, int lasttype)
+char *medusa_get_path(const struct path *path, const struct qstr *last,
+		      int lasttype)
 {
 	const char *lastname = NULL;
-	char *lastptr, *buf = NULL, *pathbuf = NULL;
-	int lastlen = 0, buflen = PATH_MAX;
+	char *lastptr;
+	char *buf = NULL;
+	char *pathbuf = NULL;
+
+	int lastlen = 0;
+	int buflen = PATH_MAX;
 
 	buf = __getname();
 	if (!buf)
 		return ERR_PTR(-ENOMEM);
 
-	/*
+	/**
+	 * DOC: Last element of the path
+	 *
 	 * d_absolute_path() prepends a path starting from the end of a buffer.
 	 * If @last element of the path is given, we should prepare a space
 	 * in the buffer for adding @last after d_absolute_path() call is made.
 	 * Ending '/' characters in @last element should be removed, as we
-	 * want obtain always all paths in the same normalised form.
+	 * always want to obtain all paths in the same normalized form.
 	 * If type of @last is '.' or '..', do not append anything to the path.
 	 */
 	if (lasttype == LAST_NORM) {
@@ -155,25 +161,29 @@ char *medusa_get_path(const struct path *path, const struct qstr *last, int last
 }
 
 /**
- * medusa_path_access - make decision about path redirection
- * @action: path access type string descripion ('mkdir', 'unlink', 'symlink', ...)
- * @path_to_redirect: in/out param; stores pointer to buffer, where is path (string),
- *	which is object of decision. If shouldn't be realised redirection,
- *	@path_to_redirect must be set to NULL using medusa_put_path() function call.
- *	This call will free buffer in correct way and set its argument to NULL.
- *	If @path_to_redirect on return is not NULL, underlaying path string is used
- *	for redirection to it.
+ * medusa_path_access() - make decision about path redirection
+ * @action: path access type string descripion ('mkdir', 'unlink', 'symlink',
+ *          ...)
+ * @path_to_redirect: in/out param; stores pointer to a buffer, where a path is
+ *      stored (string), which is the object of decision. If redirection
+ *      shouldn't be relized, @path_to_redirect must be set to %NULL using
+ *      medusa_put_path() function call. This call will free buffer in correct
+ *      way and set its argument to %NULL. If @path_to_redirect on return is not
+ *      %NULL, underlaying path string is used for redirection to it.
  *
- * Returns decision answer, which can be one of:
- *	MED_FAKE_ALLOW - after return force terminating callers function with SUCCESS
- *	MED_DENY - after return deny executing callers function
- *	other values(s), i.e. MED_ALLOW - after return allow executing callers function
+ * Return: Decision answer, which can be one of:
+ *         * %MED_FAKE_ALLOW - force caller function to terminate with a SUCCESS
+ *               after return
+ *         * %MED_DENY - deny executing caller function after return
+ *         * other values(s), i.e. %MED_ALLOW - allow executing caller function
+ *               after return
  *
  * After returning to caller:
- *	If return value is not (MED_DENY || MED_FAKE_ALLOW) and @path_to_redirect is
- *	not NULL, caller's path access is redirected to path pointed by @path_to_redirect
- *	output parameter. If return value is MED_DENY or MED_FAKE_ALLOW, value of output
- *	parameter @path_to_redirect *must be* NULL.
+ *      If return value is not (%MED_DENY || %MED_FAKE_ALLOW) and
+ *      @path_to_redirect is not %NULL, caller's path access is redirected to
+ *      path pointed by @path_to_redirect output parameter. If return value is
+ *      %MED_DENY or %MED_FAKE_ALLOW, value of output parameter
+ *      @path_to_redirect *must be* %NULL.
  */
 medusa_answer_t medusa_path_access(const char *action, char **path_to_redirect)
 {
@@ -181,16 +191,17 @@ medusa_answer_t medusa_path_access(const char *action, char **path_to_redirect)
 	//struct process_kobject process;
 	medusa_answer_t retval;
 
-	if (!strncmp(*path_to_redirect, "/tmp/medusa_path", strlen("/tmp/medusa_path"))) {
-		printk("MEDUSA PATH_ACCESS 1: act='%s', path_to_redirect='%s'", action, *path_to_redirect);
+	if (strncmp(*path_to_redirect, "/tmp/medusa_path",strlen("/tmp/medusa_path")) == 0) {
+		med_pr_debug("MEDUSA PATH_ACCESS 1: act='%s', path_to_redirect='%s'",
+			     action, *path_to_redirect);
 
-		if (!strcmp(*path_to_redirect, "/tmp/medusa_path_deny"))
+		if (strcmp(*path_to_redirect, "/tmp/medusa_path_deny") == 0)
 			retval = MED_DENY;
-		else if (!strcmp(*path_to_redirect, "/tmp/medusa_path_allow"))
+		else if (strcmp(*path_to_redirect, "/tmp/medusa_path_allow") == 0)
 			retval = MED_ALLOW;
-		else if (!strcmp(*path_to_redirect, "/tmp/medusa_path_fake_allow"))
+		else if (strcmp(*path_to_redirect, "/tmp/medusa_path_fake_allow") == 0)
 			retval = MED_FAKE_ALLOW;
-		else if (!strcmp(*path_to_redirect, "/tmp/medusa_path_redirect")) {
+		else if (strcmp(*path_to_redirect, "/tmp/medusa_path_redirect") == 0) {
 			retval = MED_ALLOW;
 			medusa_put_path(path_to_redirect);
 			*path_to_redirect = medusa_get_path_from_string("/tmp/redirected");
@@ -198,8 +209,8 @@ medusa_answer_t medusa_path_access(const char *action, char **path_to_redirect)
 		}
 		medusa_put_path(path_to_redirect);
 out:
-		printk("MEDUSA PATH_ACCESS 2: act='%s', path_redirected_to='%s', retval=%d\n", \
-			action, *path_to_redirect, retval);
+		med_pr_debug("MEDUSA PATH_ACCESS 2: act='%s', path_redirected_to='%s', retval=%d\n",
+			     action, *path_to_redirect, retval);
 		return retval;
 	}
 
