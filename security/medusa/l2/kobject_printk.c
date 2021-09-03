73b36878264f1451550e2a941d66fe073e5966e6
Roderik Ploszek <roderik.ploszek@gmail.com>
2021-09-03 10:44:00 +0200

    style: checkpatch on kobject_printk.c

--- security/medusa/l2/kobject_printk.c
+++ security/medusa/l2/kobject_printk.c
@@ -11,7 +11,7 @@ struct printk_kobject {
 };
 
 MED_ATTRS(printk_kobject) {
-	MED_ATTR_KEY	(printk_kobject, message, "message", MED_STRING),
+	MED_ATTR_KEY(printk_kobject, message, "message", MED_STRING),
 	MED_ATTR_END
 };
 
@@ -22,7 +22,7 @@ static struct medusa_kobject_s *printk_fetch(struct medusa_kobject_s *key_obj)
 static medusa_answer_t printk_update(struct medusa_kobject_s *kobj)
 {
 	((struct printk_kobject *) kobj)->message[sizeof(((struct printk_kobject *) kobj)->message)-1] = '\0';
-	med_pr_info("%s",((struct printk_kobject *) kobj)->message);
+	med_pr_info("%s", ((struct printk_kobject *) kobj)->message);
 	return MED_ALLOW;
 }
 
@@ -42,7 +42,8 @@ static int printk_kobject_unload_check(void) __exit;
 
 void printk_kobject_rmmod(void);
 
-int __init printk_kobject_init(void) {
+int __init printk_kobject_init(void)
+{
 #ifdef MODULE
 	THIS_MODULE->can_unload = printk_kobject_unload_check;
 #endif
