6c4f0a35519bc60b08455b3d1e2a9ccade11b9c0
Matus Jokay <matus.jokay@gmail.com>
2021-10-25 19:01:03 +0200

    style: remove unnecessary includes from l1/medusa.c

--- security/medusa/l1/medusa.c
+++ security/medusa/l1/medusa.c
@@ -2,61 +2,7 @@
 
 #ifdef CONFIG_SECURITY_MEDUSA
 
-#include <linux/init.h>
-#include <linux/kernel.h>
-#include <linux/tracehook.h>
-#include <linux/errno.h>
-#include <linux/sched.h>
-#include <linux/security.h>
-#include <linux/xattr.h>
-#include <linux/capability.h>
-#include <linux/unistd.h>
-#include <linux/lsm_hooks.h>
-#include <linux/mm.h>
-#include <linux/mman.h>
-#include <linux/slab.h>
-#include <linux/pagemap.h>
-#include <linux/swap.h>
-#include <linux/spinlock.h>
-#include <linux/syscalls.h>
-#include <linux/file.h>
-#include <linux/fdtable.h>
-#include <linux/msg.h>
-#include <linux/namei.h>
-#include <linux/mount.h>
-#include <linux/proc_fs.h>
-#include <linux/netfilter_ipv4.h>
-#include <linux/netfilter_ipv6.h>
-#include <linux/tty.h>
-#include <net/icmp.h>
-#include <net/ip.h> /* for local_port_range[] */
-#include <net/tcp.h> /* struct or_callable used in sock_rcv_skb */
-#include <net/net_namespace.h>
-#include <net/netlabel.h>
-#include <linux/uaccess.h>
-#include <asm/ioctls.h>
-#include <linux/atomic.h>
-#include <linux/bitops.h>
-#include <linux/interrupt.h>
-#include <linux/netdevice.h> /* for network interface checks */
-#include <linux/netlink.h>
-#include <linux/tcp.h>
-#include <linux/udp.h>
-#include <linux/dccp.h>
-#include <linux/quota.h>
-#include <linux/un.h> /* for Unix socket types */
-#include <net/af_unix.h> /* for Unix socket types */
-#include <linux/parser.h>
-#include <linux/nfs_mount.h>
-#include <net/ipv6.h>
-#include <linux/hugetlb.h>
-#include <linux/personality.h>
-#include <linux/sysctl.h>
-#include <linux/audit.h>
-#include <linux/string.h>
-#include <linux/mutex.h>
-#include <linux/posix-timers.h>
-#include <linux/cred.h>
+#include <linux/module.h>
 
 #include "l4/comm.h"
 #include "l3/registry.h"
